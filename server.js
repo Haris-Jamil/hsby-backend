cbbb57d76e3a61a4f34d6d29d7b267461f50954e
haris.jamil <haris.jamil@colwiz.com>
2023-12-25 19:32:48 +0500

    allow preflight calls cors

--- server.js
+++ server.js
@@ -11,8 +11,16 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.options("*", cors());
+app.use(
+  cors({
+    origin:
+      "https://65898d35e2582cef59e19c72--astounding-sundae-81d624.netlify.app/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(cookieParser());
-app.use(cors());
 app.use(express.json());
 app.use("/api/order", orderRouter);
 app.use("/api/user", userRouter);
