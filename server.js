e76b29ce14352d4731b9591cf58223b593abd8c3
haris.jamil <haris.jamil@colwiz.com>
2023-12-25 19:17:21 +0500

    allow cords

--- server.js
+++ server.js
@@ -13,14 +13,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin:
-      "https://65898d35e2582cef59e19c72--astounding-sundae-81d624.netlify.app/",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    optionsSuccessStatus: 204,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api/order", orderRouter);
 app.use("/api/user", userRouter);
